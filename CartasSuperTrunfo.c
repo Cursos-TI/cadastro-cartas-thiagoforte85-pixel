636e8125401e19a41e9e7ef139663abf6b8cedce
thiagoforte85-pixel <thiagoforte85@gmail.com>
2025-12-24 15:06:21 +0000

    Criei as informações da carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,39 @@
 int main() {
   printf("Desafio Super_Trufo_2025");
 
+   char estado[50];
+   char codigodacarta[50];
+   char nomedacidade[50];
+   int populacao;
+   float km;
+   float pib;
+   int ponto;
+
+   printf("Digite o Estado:\n");
+   scanf("%s", estado);
+ 
+   printf("Digite o Codigo da carta:\n");
+   scanf("%s", codigodacarta);
+
+   printf("Digite o Nome da Cidade:\n");
+   scanf("%s", nomedacidade);
+
+   printf("Digite a População:\n");
+   scanf("%d", &populacao);
+
+   printf("Digite o KM2:\n");
+   scanf("%f", km);
+
+   printf("Digite o PIB:\n");
+   scanf("%f", pib);
+   
+   printf("Digite o Ponto Turistico:\n");
+   scanf("%f", ponto);
+
+
+
+
+
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
   // Área para entrada de dados
