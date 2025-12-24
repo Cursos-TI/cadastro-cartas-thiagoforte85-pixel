3356f1866c38748643253db1ddabd13848c4f54d
thiagoforte85-pixel <thiagoforte85@gmail.com>
2025-12-24 15:43:06 +0000

    Carta A Finalizada

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,12 +5,15 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  printf("Desafio Super_Trufo_2025");
+  printf("Desafio Super_Trufo_2025\n");
+
+  printf("CARTA A:\n");
+
 
    char estado[50];
    char codigodacarta[50];
    char nomedacidade[50];
-   int populacao;
+   float populacao;
    float km;
    float pib;
    int ponto;
@@ -25,20 +28,29 @@ int main() {
    scanf("%s", nomedacidade);
 
    printf("Digite a População:\n");
-   scanf("%d", &populacao);
+   scanf("%f", &populacao);
 
    printf("Digite o KM2:\n");
    scanf("%f", km);
 
    printf("Digite o PIB:\n");
    scanf("%f", pib);
+
+   printf("Digite o numero de pontos turisticos:\n");
+   scanf("%d", &ponto);
    
-   printf("Digite o Ponto Turistico:\n");
-   scanf("%f", ponto);
+     
+   printf("O Estado: %s \n", estado);
+   printf("O Codigo da Carta: %s \n", codigodacarta);
+   printf("O Nome da Cidade: %s \n", nomedacidade);
+   printf("A população: %f \n", populacao);
+   printf("Área em km2: %f \n", km);
+   printf("O PIB: %f \n", pib);
+   printf("Número de pontos turisticos: %d \n", ponto);
 
 
 
-
+   
 
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
